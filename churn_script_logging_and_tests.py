1b006b5ba81914c9408ba802b3aa201127e841ad
EvidenceN <33920231+EvidenceN@users.noreply.github.com>
2021-11-15 09:29:54 -0700

    project done except adding readme

--- churn_script_logging_and_tests.py
+++ churn_script_logging_and_tests.py
@@ -35,36 +35,71 @@ def test_import(data):
 		raise err
 
 
-def test_eda(perform_eda):
+def test_eda(eda):
 	'''
 	test perform eda function
 	'''
-	pass
+	df = import_data("./data/bank_data.csv")
+	try:
+		eda(df)
+		logging.info("Images saved successfully")
+	except:
+		logging.error("Testing perform_eda: The images were not saved")
+	# Don't know how to do assert statement to check the results
 
 
 def test_encoder_helper(encoder_helper):
 	'''
 	test encoder helper
 	'''
-	pass
+	df = import_data("./data/bank_data.csv")
+	cat_columns = ['Gender','Education_Level', 'Marital_Status','Income_Category', 'Card_Category']
+	response = "Churn"
+	try:
+		encoder_helper(df, cat_columns, response)
+		logging.info("Encoding was successful")
+	except:
+		logging.error("Testing encoding_helper: categorical columns were not successfully encoded")
+	# Don't know how to do assert statement to check the results
 
+def test_cols_to_keep(cols_to_keep):
+	'''
+	test cols_to_keep
+	'''
+	df = import_data("./data/bank_data.csv")
+	cat_columns = ['Gender','Education_Level', 'Marital_Status','Income_Category', 'Card_Category']
+	response = "Churn"
+	df2 = encoder_helper(df, cat_columns, response)
+	try:
+		cols_to_keep (df2)
+		logging.info("Feature engineering was successful")
+	except:
+		logging.error("Testing feature_engineering: columns were not transformed")
+	# Don't know how to do assert statement to check the results
 
-def test_perform_feature_engineering(perform_feature_engineering):
+def test_train_models(training_models):
 	'''
 	test perform_feature_engineering
 	'''
-	pass
-
-
-def test_train_models(train_models):
-	'''
-	test train_models
-	'''
-	pass
+	df = import_data("./data/bank_data.csv")
+	cat_columns = ['Gender','Education_Level', 'Marital_Status','Income_Category', 'Card_Category']
+	response = "Churn"
+	df2 = encoder_helper(df, cat_columns, response)
+	X, y = cols_to_keep (df2)
+	try:
+		training_models(X, y)
+		logging.info("Model split into train and test was successful")
+	except:
+		logging.error("test_train_models: Training and test data was not split sucessfully")
+	# Don't know how to do assert statement to check the results	
 
 
 if __name__ == "__main__":
 	test_import(import_data)
+	test_eda(perform_eda)
+	test_encoder_helper(encoder_helper)
+	test_cols_to_keep(cols_to_keep)
+	test_train_models(perform_feature_engineering)
 
 
 
